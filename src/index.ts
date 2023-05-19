22e6f3bcf06bd4deb99dfb380c8a38fe0a01b57b
inokawa <48897392+inokawa@users.noreply.github.com>
2023-05-19 20:40:38 +0900

    Update monaco-editor-core

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
+import type { editor } from "monaco-editor-core";
 // @ts-expect-error no type definition
-import { DiffComputer } from "monaco-editor-core/esm/vs/editor/common/diff/diffComputer";
+import { DiffComputer } from "monaco-editor-core/esm/vs/editor/common/diff/smartLinesDiffComputer";
+
+export type ILineChange = editor.ILineChange;
+export type ICharChange = editor.ICharChange;
 
 export type Opts = {
   shouldComputeCharChanges?: boolean;
@@ -7,27 +11,6 @@ export type Opts = {
   shouldIgnoreTrimWhitespace?: boolean;
   shouldMakePrettyDiff?: boolean;
   maxComputationTime?: number;
-};
-
-// https://microsoft.github.io/monaco-editor/api/interfaces/monaco.editor.ILineChange.html
-export type ILineChange = {
-  readonly charChanges?: ICharChange[];
-  readonly modifiedEndLineNumber: number;
-  readonly modifiedStartLineNumber: number;
-  readonly originalEndLineNumber: number;
-  readonly originalStartLineNumber: number;
-};
-
-// https://microsoft.github.io/monaco-editor/api/interfaces/monaco.editor.ICharChange.html
-export type ICharChange = {
-  readonly modifiedEndColumn: number;
-  readonly modifiedEndLineNumber: number;
-  readonly modifiedStartColumn: number;
-  readonly modifiedStartLineNumber: number;
-  readonly originalEndColumn: number;
-  readonly originalEndLineNumber: number;
-  readonly originalStartColumn: number;
-  readonly originalStartLineNumber: number;
 };
 
 export default (
