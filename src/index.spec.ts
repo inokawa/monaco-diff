259132b84a63266b62a4af81ef6c2f672fd53123
inokawa <48897392+inokawa@users.noreply.github.com>
2023-05-19 20:58:41 +0900

    Add linesDiff

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "@jest/globals";
-import diff from "../lib";
+import { diff, legacyLinesDiff, linesDiff } from "../lib";
 
 describe("computeDiff", () => {
   it("singleline-1", () => {
@@ -16,6 +16,60 @@ describe("computeDiff", () => {
   it("multiline-2", () => {
     expect(
       diff(
+        "This line is removed on the right.\njust some text\nabcd\nefgh\nSome more text".split(
+          "\n"
+        ),
+        "just some text\nabcz\nzzzzefgh\nSome more text.\nThis line is removed on the left.".split(
+          "\n"
+        )
+      )
+    ).toMatchSnapshot();
+  });
+});
+
+describe("compute legacy line diff", () => {
+  it("singleline-1", () => {
+    expect(
+      legacyLinesDiff(["heLLo world!"], ["hello orlando!"])
+    ).toMatchSnapshot();
+  });
+  it("multiline-1", () => {
+    expect(
+      legacyLinesDiff(
+        "just some text\n\nHello World\n\nSome more text".split("\n"),
+        "just some Text\n\nHello World\n\nSome more changes".split("\n")
+      )
+    ).toMatchSnapshot();
+  });
+  it("multiline-2", () => {
+    expect(
+      legacyLinesDiff(
+        "This line is removed on the right.\njust some text\nabcd\nefgh\nSome more text".split(
+          "\n"
+        ),
+        "just some text\nabcz\nzzzzefgh\nSome more text.\nThis line is removed on the left.".split(
+          "\n"
+        )
+      )
+    ).toMatchSnapshot();
+  });
+});
+
+describe("compute line diff", () => {
+  it("singleline-1", () => {
+    expect(linesDiff(["heLLo world!"], ["hello orlando!"])).toMatchSnapshot();
+  });
+  it("multiline-1", () => {
+    expect(
+      linesDiff(
+        "just some text\n\nHello World\n\nSome more text".split("\n"),
+        "just some Text\n\nHello World\n\nSome more changes".split("\n")
+      )
+    ).toMatchSnapshot();
+  });
+  it("multiline-2", () => {
+    expect(
+      linesDiff(
         "This line is removed on the right.\njust some text\nabcd\nefgh\nSome more text".split(
           "\n"
         ),
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,28 @@
 import type { editor } from "monaco-editor-core";
+import {
+  DiffComputer,
+  SmartLinesDiffComputer as LinesDiffComputer,
+  // @ts-expect-error no type definition
+} from "monaco-editor-core/esm/vs/editor/common/diff/smartLinesDiffComputer.js";
 // @ts-expect-error no type definition
-import { DiffComputer } from "monaco-editor-core/esm/vs/editor/common/diff/smartLinesDiffComputer";
+import { StandardLinesDiffComputer } from "monaco-editor-core/esm/vs/editor/common/diff/standardLinesDiffComputer.js";
 
 export type ILineChange = editor.ILineChange;
 export type ICharChange = editor.ICharChange;
+export type LineRangeMapping = editor.LineRangeMapping;
 
-export type Opts = {
+export interface DiffOpts {
   shouldComputeCharChanges?: boolean;
   shouldPostProcessCharChanges?: boolean;
   shouldIgnoreTrimWhitespace?: boolean;
   shouldMakePrettyDiff?: boolean;
   maxComputationTime?: number;
-};
+}
 
-export default (
+export const diff = (
   originalLines: string[],
   modifiedLines: string[],
-  opts: Opts = {}
+  opts: DiffOpts = {}
 ): ILineChange[] => {
   return new DiffComputer(originalLines, modifiedLines, {
     shouldComputeCharChanges: true,
@@ -26,4 +32,37 @@ export default (
     maxComputationTime: 5000,
     ...opts,
   }).computeDiff().changes;
+};
+
+interface LinesDiffOpts {
+  ignoreTrimWhitespace?: boolean;
+  maxComputationTimeMs?: number;
+}
+
+export const legacyLinesDiff = (
+  originalLines: string[],
+  modifiedLines: string[],
+  opts: LinesDiffOpts = {}
+): LineRangeMapping[] => {
+  return new LinesDiffComputer().computeDiff(originalLines, modifiedLines, {
+    ignoreTrimWhitespace: false,
+    maxComputationTimeMs: 1000,
+    ...opts,
+  }).changes;
+};
+
+export const linesDiff = (
+  originalLines: string[],
+  modifiedLines: string[],
+  opts: LinesDiffOpts = {}
+): LineRangeMapping[] => {
+  return new StandardLinesDiffComputer().computeDiff(
+    originalLines,
+    modifiedLines,
+    {
+      ignoreTrimWhitespace: false,
+      maxComputationTimeMs: 1000,
+      ...opts,
+    }
+  ).changes;
 };
